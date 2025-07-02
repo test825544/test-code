33bbf2bd92eb8dd551d1338440cbcf833241131a
test825544 <143061207+test825544@users.noreply.github.com>
2025-07-02 10:54:03 +0530

    Create index.ts

---
+++ get-stocks/types/index.ts
@@ -0,0 +1,4 @@
+export * from './get-stocks-path-parameters';
+export * from './get-stocks-query-parameters';
+export * from './get-stocks-marshalled-response';
+export * from './get-stocks-response';
